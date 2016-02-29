1d3e4ce5c856322e31a8e4690eed153acd419156
rafpacut <rafpacut@gmail.com>
2016-02-29 22:35:18 +0100

    Image url extraction from note text

--- ctrl/notesCtrl.js
+++ ctrl/notesCtrl.js
@@ -3,10 +3,21 @@ var app = angular.module('notesList', [] );
 app.controller('notes_ctrl', ['$scope', function( $scope ) {
 
 		$scope.notes = [];	
+		$scope.images_src = [];
 
 		$scope.addNote = function(note) {
+			detect_images(note);
 			$scope.notes.push(note);
+			console.log($scope.images_src);
 		};
+
+		function detect_images(note) {
+			
+			var imgRegex = /https?:\/\/.*\.(?:png|jpg)/i;
+			var imgSources = note.match(imgRegex);
+			$scope.images_src.push(imgSources);
+
+		}
 
 
 }]);
