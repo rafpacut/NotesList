0a56462cb60f8c22d1e92dcf86ccf656ed53de96
rafpacut <rafpacut@gmail.com>
2016-02-29 22:53:37 +0100

    Storing image sources with its note

--- ctrl/notesCtrl.js
+++ ctrl/notesCtrl.js
@@ -2,22 +2,25 @@ var app = angular.module('notesList', [] );
 
 app.controller('notes_ctrl', ['$scope', function( $scope ) {
 
-		$scope.notes = [];	
-		$scope.images_src = [];
-
-		$scope.addNote = function(note) {
-			detect_images(note);
-			$scope.notes.push(note);
-			console.log($scope.images_src);
-		};
-
-		function detect_images(note) {
+		function detectImages(note) {
 			
 			var imgRegex = /https?:\/\/.*\.(?:png|jpg)/i;
 			var imgSources = note.match(imgRegex);
-			$scope.images_src.push(imgSources);
-
+			return imgSources;
 		}
+
+		$scope.notes = [];	
+
+		$scope.addNote = function(noteText) {
+			var note = { 
+				text: noteText,
+				imgSources: detectImages(noteText)
+			}
+
+			$scope.notes.push(note);
+			console.log($scope.notes);
+		};
+
 
 
 }]);
